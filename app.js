1ee5b0ddd022c88b1ca2f4007fff2345b830e342
Zeeshan Ahmad <zeeshanamd256@gmail.com>
2023-10-31 23:14:00 +0500

    module2-solution.

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 // app.js
-angular.module('LunchCheckerApp', [])
-.controller('LunchCheckerController', function($scope) {
-    $scope.checkLunch = function() {
-        // Implement your logic to check the lunch input and set the message here
-        // For example:
-        if ($scope.lunchMenu) {
-            var dishes = $scope.lunchMenu.split(',');
-            if (dishes.length <= 3) {
-                $scope.message = 'Enjoy!';
-            } else {
-                $scope.message = 'Too much!';
-            }
-        } else {
-            $scope.message = 'Please enter a list of dishes.';
-        }
-    };
-});
+(function () {
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ShoppingListController', ShoppingListController);
+
+    ShoppingListController.$inject = [];
+
+    function ShoppingListController() {
+        var controller = this;
+
+        controller.toBuyList = [
+            { name: 'cookies', quantity: 10 },
+            { name: 'apples', quantity: 5 },
+            { name: 'bananas', quantity: 8 },
+            { name: 'milk', quantity: 2 },
+            { name: 'bread', quantity: 1 }
+        ];
+
+        controller.alreadyBoughtList = [];
+
+        controller.buyItem = function (index) {
+            var item = controller.toBuyList.splice(index, 1)[0];
+            controller.alreadyBoughtList.push(item);
+        };
+    }
+})();
